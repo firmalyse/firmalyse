ed2c516bba716d3bc530bc17eb156dddfeba34e6
Zachary <zacharytang.tc@gmail.com>
2019-03-22 07:58:07 -0700

    add file lmao

---
+++ src/analysis/CheckPasswords.py
@@ -0,0 +1,51 @@
+"""
+Checks if any password files found have weak passwords in them
+"""
+
+import os
+
+class CheckPasswords:
+
+	def __init__(self, firmwareFolder):
+		"""
+		firmwareFolder: folder name of the extracted firmware
+		"""
+		self.firmwareFolder = firmwareFolder
+
+	def runChecks(self):
+		pwdFiles = self._getPwdFiles()
+
+	def _getPwdFiles(self):
+		with open("analysis_result/firmwalkerOutput.txt", "r") as f:
+			pwdFiles = list()
+
+			line = f.readline().strip()
+			# read till start of list
+			while not "##################################### passwd" in line:
+				line = f.readline().strip()
+			line = f.readline().strip()
+
+			while line != "":
+				pwdFiles.append(line)
+				line = f.readline().strip()
+			
+                        while not "##################################### shadow" in line:
+				line = f.readline().strip()
+			line = f.readline().strip()
+
+			while line != "":
+				pwdFiles.append(line)
+				line = f.readline().strip()
+			
+			while not "##################################### *.psk" in line:
+				line = f.readline().strip()
+			line = f.readline().strip()
+
+			while line != "":
+				pwdFiles.append(line)
+				line = f.readline().strip()
+
+
+		return pwdFiles
+
+
