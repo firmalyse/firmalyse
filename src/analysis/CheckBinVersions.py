67f4916493785250056eddd8c3751beda1f18d4f
yapni <yapni8@gmail.com>
2019-04-13 00:04:59 +0800

    Update CheckBinVersions to check Telnet and BusyBox versions

--- src/analysis/CheckBinVersions.py
+++ src/analysis/CheckBinVersions.py
@@ -16,8 +16,9 @@ class CheckBinVersions:
     def run(self):
         self.findFiles()
 
-        if self.openssl != '':
-            openSSLIssues = self.checkOpenSSL()
+        self.checkOpenSSL()
+        self.checkTelnet()
+        self.checkBusyBox()
 
     
     def checkOpenSSL(self):
@@ -26,13 +27,16 @@ class CheckBinVersions:
         to the heartbleed attack.
         """
         
-        issues = {'Heartbleed': False, 'Outdated': False}
+        issues = {'Present': False, 'Heartbleed': False, 'Outdated': False}
+
+        if self.openssl == '': 
+            return issues
+        
+        issues['Present'] = True
 
         relativePath = 'analysis_result/' + self.firmwareFolder + '/' + self.openssl
         versionStr = subprocess.check_output('strings -n 10 ' + relativePath + '| grep \"OpenSSL\"', shell=True).split('\n')[-2]
         versionNum = versionStr.split(' ')[1]
-
-        print(versionNum)
 
         # Check if the version of OpenSSL is vulnerable to the heartbleed attack
         if '1.0.1' in versionNum:
@@ -44,6 +48,53 @@ class CheckBinVersions:
                 if versionNum[:-1] in line:
                     issues['Outdated'] = True
                     break
+
+        print("OpenSSL: {}".format(issues))
+
+        return issues
+    
+    def checkBusyBox(self):
+        """
+        Checks if there is a vulnerable version of BusyBox in the firmware.
+        """
+
+        issues = {'Present': False, 'VulnerableVersion': False}
+
+        if self.busyBox == '':
+            return issues
+
+        issues['Present'] = True
+
+        relativePath = 'analysis_result/' + self.firmwareFolder + '/' + self.busyBox
+        keywordsArray = subprocess.check_output('strings ' + relativePath + '| grep -P \"^BusyBox v([0-9])+\.([0-9])+.+\"', shell=True)\
+                        .split('\n')
+        keywordsArray = filter(None, keywordsArray) # Filter out empty string since subprocess.check_output throws out extra newline
+        versionNum = keywordsArray[0].split(' ')[1].strip('v') # Just take the 1st result of grep as the target version string
+
+        print("Version caught: {}".format(versionNum))
+        
+        # Check if the version of BusyBox is vulnerable
+        with open('analysis/data/BusyBoxVulnerableVersions', 'r') as vulnerableVersions:
+            for line in vulnerableVersions:
+                if versionNum in line:
+                    issues['VulnerableVersion'] = True
+                    break
+        
+        print("BusyBox: {}".format(issues))
+        
+        return issues
+
+    def checkTelnet(self):
+        """
+        Checks if telnet is present in the firmware.
+        """
+
+        issues = {'InsecureTelnetPresent': False}
+
+        if self.telnet != '':
+            issues['InsecureTelnetPresent'] = True
+
+        print("Telnet: {}".format(issues))
 
         return issues
 
